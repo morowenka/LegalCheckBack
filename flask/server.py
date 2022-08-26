37f3dbca410a5d5b57176b003522bf82c7a1e643
s70c3 <mari.s70c3@gmail.com>
2022-08-26 23:36:26 +0300

    Initial commit

---
+++ flask/server.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import os
+
+from flask import Flask, request
+from pymongo import MongoClient
+from bson.json_util import dumps
+
+app = Flask(__name__)
+
+client = MongoClient("mongo:27017")
+db = client.ArticleDB
+#
+@app.route('/', methods = ['GET'])
+def todo():
+    try:
+        client.admin.command('ismaster')
+    except:
+        return "Server not available"
+    return "Hello from the MongoDB client!\n"
+
+
+@app.route("/insert_article",methods=['POST'])
+def insert():
+    article = request.form.get('article')
+    try:
+       db.articles.insert_one(
+              {
+                     'article': article,
+              }
+       )
+       return dumps({'message': 'SUCCESS'})
+    except Exception as e:
+       return dumps({'error': str(e)})
+
+@app.route("/get_all_articles", methods = ['GET'])
+def get_all_articles():
+    try:
+        articles = db['articles'].find()
+        # print(contacts.name)
+        return dumps(articles)
+    except Exception as e:
+        return dumps({'error' : str(e)})
+
+@app.route("/get_one_article/<name>", methods = ['GET'])
+def get_one_article(name):
+    try:
+        x = db['articles'].find_one({"name": name})
+        return dumps(x)
+    except Exception as e:
+        return dumps({'error' : str(e)})
+
+
+if __name__ == "__main__":
+
+    app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
+
